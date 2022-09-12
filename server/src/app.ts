397aa49b487f8b03de492253faf5ed309aa7d1af
Isaac Olanrewaju <isaacolanre@gmail.com>
2022-09-12 21:13:46 +0100

    set up with eslint-prettier-husky

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,20 @@
-import createError, { HttpError } from 'http-errors'
-import express,{Request, Response, NextFunction} from 'express'
-import path from 'path'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
-import db from './config/database.config'
+import createError, { HttpError } from 'http-errors';
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import db from './config/database.config';
 
 // import indexRouter from './routes/index'
 // import usersRouter from './routes/users'
 
-
-db.sync().then(()=>{
-  console.log("Database connected succefully 🚀")
-}).catch(err=>{
-  console.log(err)
-})
+db.sync()
+  .then(() => {
+    console.log('Database connected succefully 🚀');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
@@ -24,18 +25,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err:HttpError, req:Request, res:Response, next:NextFunction) {
+app.use(function (err: HttpError, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
