c83c53b39e03ac37d67ea033777904d13dbb5f16
jamesobed <jamesobed4@gmail.com>
2022-09-30 17:14:10 +0100

    transaction history

--- server/src/controller/userController.ts
+++ server/src/controller/userController.ts
@@ -395,7 +395,7 @@ export async function singleUser(req: Request, res: Response, next: NextFunction
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-    return res.status(200).json({ message: 'User found', user });
+    return res.status(200).json({ message: 'User found', TransactionHistory: user.withdrawBalance });
   } catch (err) {
     return res.status(500).json({ message: 'failed to get user' });
   }
